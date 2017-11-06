77a1922c81fb6b978c95ba492c5fff7dbdd23020
hechunyan <1289630384@qq.com>
2017-11-06 16:06:51 +0800

    update

--- src/main/python/useraction/deal_es_status.py
+++ src/main/python/useraction/deal_es_status.py
@@ -7,39 +7,14 @@ Crate on 1 Nov 2017
 from elasticsearch import Elasticsearch
 import json
 from datetime import datetime
-from deal_es import init_es
 
-def deal_status_rsstatus(es_data):
-    source_data = []
-    for hit in es_data['hits']['hits']:
-        datalen = len(es_data['hits']['hits'])
-        for i in range(datalen):
-            if es_data['hits']['hits'][i]['_source']['rsstatus'] == 0:
-                sourcedt = es_data['hits']['hits'][i]['_source']
-            source_data.append(sourcedt)
-    return source_data
-
-def up_rsstatus(es_data):
-    dtlen = len(es_data)
-    for i in range(dtlen):
-        if es_data[i]['rsstatus'] == 0:
-            es_data[i]['rsstatus'] = 1
-    return es_data
-
-def init_step():
-    es = init_es()
-    doc = {
-        "step":0,
-        "timestamp":datetime.now()
-    }
-    es_res = es.indices.create(index = "step", body = doc)
-    return es_res
-
-def get_step():
-    es = init_es()
-    res = es.search(index = 'step', body={"query": {"match_all": {}}})
-    return res
-
+def deal_es_timestamp(es_data):
+    #es_data = ds.get_es_data()
+    datalen = len(es_data['hits']['hits'])
+    data = es_data['hits']['hits'][datalen -1]
+    datedata = data["_source"]['@timestamp']
+    dt = datetime.strptime(datedata, "%Y-%m-%dT%H:%M:%S.%fZ")
+    return dt
 
 if __name__ == '__main__':
 	pass
--- src/unittest/python/test_deal_status/deal_status_test.py
+++ src/unittest/python/test_deal_status/deal_status_test.py
@@ -5,7 +5,7 @@ Crate on 2 Nov 2017
 '''
 
 import unittest
-from useraction import deal_status as ds
+from useraction import deal_es_status as ds
 import data_of_test as td
 
 class MyTestCase(unittest.TestCase):
@@ -14,14 +14,9 @@ class MyTestCase(unittest.TestCase):
         self.deal_status_data = td.deal_es_data()
         self.up_status_data = td.up_es_data()
 
-    def test_deal_status(self):
+    def test_deal_es_status(self):
         self.set_data()
-        print (ds.deal_status(self.deal_status_data))
-
-    def test_up_status(self):
-        self.set_data()
-        print (ds.up_rsstatus(self.up_status_data))
-
+        print (ds.deal_es_timestamp(self.deal_status_data))
 
 if __name__ == '__main__':
     unittest.main()
